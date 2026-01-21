477707f4584768140b55635bdcf9e575c0bb309f
Adit Rustagi <144267879+Rustadit@users.noreply.github.com>
2026-01-21 14:08:04 +0530

    Update uber_questions.sql

--- uber_questions.sql
+++ uber_questions.sql
@@ -123,12 +123,13 @@
 --Q.4 Calculate the Month-over-Month (MoM) Growth Percentage in total completed ride volume to identify months with significant demand spikes or dips.
 
     --table of current month and previous month completed rides
-    WITH month_wise_rides AS (SELECT date_trunc('MONTH',request_ts) month_trunc,
-    	  					  COUNT(*) no_of_completed_rides,
-    	  					  LAG(COUNT(*)) OVER (ORDER BY date_trunc('MONTH',request_ts)) previous_month_completed_rides
-    	  					  FROM rides
-    	  				      WHERE status='Completed'
-    	  					  GROUP BY 1)
+    WITH month_wise_rides AS 
+		(SELECT date_trunc('MONTH',request_ts) month_trunc,
+    	        COUNT(*) no_of_completed_rides,
+    			LAG(COUNT(*)) OVER (ORDER BY date_trunc('MONTH',request_ts)) previous_month_completed_rides
+    	 FROM rides
+    	 WHERE status='Completed'
+    	 GROUP BY 1)
 
     SELECT TO_CHAR(month_trunc, 'MONTH') AS month_name,
     	   no_of_completed_rides,
@@ -361,5 +362,6 @@
     JOIN rides r
     ON apps.session_id=r.session_id
     GROUP BY 1
+
 
 
